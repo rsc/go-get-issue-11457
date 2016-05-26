041f0ee581ae46127516a4015d3c73156165641c
Dmitri Shuralyov <shurcooL@gmail.com>
2016-05-25 21:58:44 -0700

    Initial commit.

---
+++ foo.go
@@ -0,0 +1,4 @@
+// Empty package to test go get for https://golang.org/issues/11457.
+//
+// This package must have an import comment (which is valid) for the test to be functional.
+package foo // import "github.com/rsc/go-get-issue-11457"
