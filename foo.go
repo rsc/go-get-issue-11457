b0d8c97b72e5371d36e9cfa8b783b52260914af0
Russ Cox <rsc@golang.org>
2016-11-02 23:18:17 -0400

    no need for import comment

--- foo.go
+++ foo.go
@@ -1,4 +1,2 @@
 // Empty package to test go get for https://golang.org/issues/11457.
-//
-// This package must have an import comment (which is valid) for the test to be functional.
-package foo // import "rsc.io/go-get-issue-11457"
+package foo
