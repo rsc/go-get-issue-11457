9c954af6aca1463d5e149a860354980109459260
Russ Cox <rsc@golang.org>
2016-11-02 23:16:51 -0400

    use rsc.io

--- foo.go
+++ foo.go
@@ -1,4 +1,4 @@
 // Empty package to test go get for https://golang.org/issues/11457.
 //
 // This package must have an import comment (which is valid) for the test to be functional.
-package foo // import "github.com/rsc/go-get-issue-11457"
+package foo // import "rsc.io/go-get-issue-11457"
